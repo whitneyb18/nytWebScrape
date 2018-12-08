af25bdf7186f84e07f527f2e162285dfd7eab410
Gold Leader <whitneyb18@gmail.com>
2018-12-08 12:28:34 -0700

    PORT really fixed this time

--- server.js
+++ server.js
@@ -12,8 +12,10 @@ var cheerio = require("cheerio");
 var db = require("./models");
 
 var PORT = process.env.PORT || 3000;
+
 var MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
 
 // Initialize Express
 var app = express();
